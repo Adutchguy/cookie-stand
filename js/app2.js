626e901878d1dd0d320ee9069cc8234bc412256b
Adutchguy <mdm_8@hotmail.com>
2017-03-14 16:35:18 -0700

    I don't even know.

--- js/app2.js
+++ js/app2.js
@@ -2,9 +2,10 @@
 
 var hours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm', '8pm'];
 
-var firstAndPike;
+var storeName = ['1st and Pike', 'SeaTac Airport', 'Seattle Center', 'Capitol Hill', 'Alki'];
 
-var creatStore = function(name,minCust,maxCust,avgCookies,location) {
+var creatStores = function(name,minCust,maxCust,avgCookies,location) {
+
   this.name = name,
 
   this.minCust = minCust,
@@ -21,7 +22,7 @@ var creatStore = function(name,minCust,maxCust,avgCookies,location) {
     return Math.ceil(this.randomCustomerPerHr() * this.avgCookies);
   },
 
-  this.cookiesSoldEachHr = function() {
+  this.cookiesByHour = function() {
     var cookiesPerHour = [];
     for (var i = 0 ; i < hours.length ; i++) {
       cookiesPerHour.push(this.randomCookiesPurchHr());
@@ -32,7 +33,7 @@ var creatStore = function(name,minCust,maxCust,avgCookies,location) {
   this.daysTotalCookies = function() {
     var total = 0;
     for (var i = 0 ; i < hours.length ; i++) {
-      total += this.cookiesSoldEachHr()[i];
+      total += this.cookiesByHour()[i];
     }
     return total;
   },
@@ -45,36 +46,62 @@ var creatStore = function(name,minCust,maxCust,avgCookies,location) {
     return hoursList;
   },
 
-  this.pushAllToArray = function() {
-    var showSummary = [];
-    for (var i = 0 ; i < hours.length ; i++) {
-      showSummary.push(this.daysHoursListed()[i] + ': ' + this.cookiesSoldEachHr()[i] + ' cookies');
+  this.getStoreNames = function() {
+    var storeNameArray = [];
+    for (var i = 0; i < storeName.length; i++) {
+      storeNameArray.push(storeName[i]);
     }
-    return showSummary;
+    return storeNameArray;
   },
 
-  this.createListItems = function() {
-    for (var i = 0 ; i < hours.length ; i++) {
-      var ul1 = document.createElement('li');
-      var newText = document.createTextNode(this.pushAllToArray()[i]);
-      ul1.appendChild(newText);
-      var position = document.getElementById(location);
-      position.appendChild(ul1);
+  this.renderTH = function() {
+    var newthead = document.createElement('thead');
+    var newth = document.createElement('th');
+    var placeNewth = document.getElementsByTagName('thead');
+    var placeNewthead = document.getElementsByTagName('table');
+    placeNewthead.appendChild(placeNewth[0]);
+    for (var i = 0; i < storeName.length; i++) {
+      var newText = document.createTextNode(getStoreNames()[i]);
+      newth.appendChild(newText);
+      newthead.appendChild(newth);
     }
-    var ul1 = document.createElement('li');
-    var newText = document.createTextNode('Total ' + this.daysTotalCookies() + 'cookies');
-    ul1.appendChild(newText);
-    var position = document.getElementById(location);
-    position.appendChild(ul1);
-  };
+  },
+
+  this.renderTD = function() {
+    for (var i = 0 ; i < hours.length ; i++) {
+      var newTD = document.createElement('td');
+      var att = document.createAttribute("scope")
+      att.value = "row";
+      newTD.setAttributeNode(att);
+      var newText = document.createTextNode(this.cookiesByHour()[i] + ' cookies');
+      newTD.appendChild(newText);
+      var placeHere = document.getElementById(location);
+      placeHere.appendChild(newTD);
+    }
+  }
+
 };
-var firstAndPike = new creatStore('firstAndPike',23,65,6.3,'one');
-firstAndPike.createListItems();
-var seatacAirport = new creatStore('seatacAirport',3,24,1.2,'two');
-seatacAirport.createListItems();
-var firstAndPike = new creatStore('seattleCenter',11,38,2.3,'three')
-firstAndPike.createListItems();
-var capitolHill = new creatStore('capitolHill',20,38,2.3,'four')
-capitolHill.createListItems();
-var alki = new creatStore('alki',2,16,4.6,'five')
-alki.createListItems();
+//     var ul1 = document.createElement('li');
+//     var newText = document.createTextNode('Total ' + this.daysTotalCookies() + 'cookies');
+//     ul1.appendChild(newText);
+//     var position = document.getElementById(location);
+//     position.appendChild(ul1);
+//   };
+// };
+//
+var firstAndPike = new creatStores('firstAndPike',23,65,6.3,'one');
+firstAndPike.renderTH();
+
+
+// 'use strict'
+//
+// var newTD = document.createElement('td');
+// console.log(newTd)
+//
+//
+// var newText = document.createTextNode('some text');
+// newTd.appendChild(newText);
+//
+// var position = document.getElementsById('bdy');
+// console.log(position);
+// position.appendChild(newTd);
